a366bec7fc583a8eb0d74e868814782bc0bfb4de
Shetty <niroop893@gmail.com>
2024-09-27 12:32:19 +0530

    update react app

--- newsmonkey/src/App.js
+++ newsmonkey/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Easy Learning React with Niroop 
         </a>
       </header>
     </div>
