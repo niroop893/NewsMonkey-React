74136a0457738f427e943bd0f26b9780cae789aa
Shetty <niroop893@gmail.com>
2024-09-28 16:05:26 +0530

    update Navbar.js

--- newsmonkey/src/App.js
+++ newsmonkey/src/App.js
@@ -1,21 +1,14 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    
     <>
-      <nav>
-      <li> Home</li>
-      <li> Contact</li>
-      <li> About</li>
-      </nav>
-    <div className='container'>
-      <h1>Lorem34 </h1>
-    </div>
-    
-    
-    </>
+      <Navbar title="TextUtils2" aboutText="About Us"/>
+      {/* <Navbar /> */}
+      {/* <Navbar title="TextUtils" /> */}
+          
+      </>
   );
 }
 
