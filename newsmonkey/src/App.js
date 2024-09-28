881892d4095a884485c530cad3427c72f54c10f4
Shetty <niroop893@gmail.com>
2024-09-28 12:31:37 +0530

    modified app.js

--- newsmonkey/src/App.js
+++ newsmonkey/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Easy Learning React with Niroop 
-        </a>
-      </header>
+    
+    <>
+      <nav>
+      <li> Home</li>
+      <li> Contact</li>
+      <li> About</li>
+      </nav>
+    <div className='container'>
+      <h1>Lorem34 </h1>
     </div>
+    
+    
+    </>
   );
 }
 
