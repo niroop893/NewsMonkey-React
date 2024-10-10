22d299c56543e3d7c091cb567fabed5d5aa6cb55
Shetty <niroop893@gmail.com>
2024-10-10 12:21:39 +0530

    Update Navbar and Textform

--- newsmonkey/src/App.js
+++ newsmonkey/src/App.js
@@ -9,18 +9,18 @@ function App() {
   const toggleMode = () => {
     if (mode === 'light'){
       setMode('dark')
-      document.body.style.backgroundColor = 'dark'
+      document.body.style.backgroundColor = 'grey'
     }
     else {
       setMode('light')
-      document.body.style.backgroundColor = 'light'
+      document.body.style.backgroundColor = 'white'
     }
   }
   return (
     <>    
       <Navbar title="TextUtils" aboutText="About Us" Contact="Contact Us" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-      <TextForm heading="Enter Text to Analysis"/>
+      <TextForm heading="Enter Text to Analysis" mode={mode} />
       {/* <About /> */}
       </div>
           
