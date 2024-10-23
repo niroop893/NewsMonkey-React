cae314415cbc091b779dbb788e9e0ec3dc2e47b8
Shetty <niroop893@gmail.com>
2024-10-23 14:08:48 +0530

    news article page size

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -9,11 +9,11 @@ export class NewsItem extends Component {
       <div>
         <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={!imageUrl?"https://techcrunch.com/wp-content/uploads/2024/05/alphafold-3-deepmind.webp?resize=1536,864":imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank" className='btn btn-sm btn-primary' >Read More</a>
+              <a rel="noreferrer" href={newsUrl} target="_blank" className='btn btn-sm btn-dark' >Read More</a>
             </div>
           </div>
           </div>
