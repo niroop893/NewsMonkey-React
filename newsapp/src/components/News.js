ad4f43553a698f9309974081f3d85cb5a839c47e
Shetty <niroop893@gmail.com>
2024-10-25 14:20:01 +0530

    newsapp update

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -16,17 +16,22 @@ export class News extends Component {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string
-  }
 
+  }
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
    
-    constructor(){
-    super();
+    constructor(props){
+    super(props);
     console.log('Hello I am consturtor for News component')
     this.state = {
       articles: [],
       loading: true,
       page: 1
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - News Monkey`
+    
   }
 
   async updateNews(){
@@ -97,7 +102,7 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1 className='text-center' style={{margin: '35px 0px'}}>NewsMonkey - TopHeadlines</h1>
+        <h1 className='text-center' style={{margin: '35px 0px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner />}
         <div className='row'>
         {!this.state.loading && this.state.articles.map((element)=>{
