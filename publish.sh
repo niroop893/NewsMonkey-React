4c1ad39d19441b2e4d7e95dd6db47f313be9ded4
Niroopkumar Shetty <niroop893@gmail.com>
2025-04-14 06:35:02 +0000

    Edit publish.sh

--- publish.sh
+++ publish.sh
@@ -5,5 +5,8 @@ export CI_PROJECT_NAME=$CI_PROJECT_NAME
 export CI_COMMIT_TAG=$CI_COMMIT_TAG
 export CI_PROJECT_ID=$CI_PROJECT_ID
 
+echo "Looking for file $CI_PROJECT_NAME-$CI_COMMIT_TAG.tgz"
+ls -l $CI_PROJECT_NAME-$CI_COMMIT_TAG.tgz
+
 curl --header "Authorization: Bearer $CI_JOB_TOKEN" --header "Content-Type: application/json" --request POST --data "{\"package\": {\"name\": \"$CI_PROJECT_NAME\", \"version\": \"$CI_COMMIT_TAG\"}}" https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/packages
 curl --header "Authorization: Bearer $CI_JOB_TOKEN" --header "Content-Type: application/octet-stream" --request PUT --data-binary "@$CI_PROJECT_NAME-$CI_COMMIT_TAG.tgz" https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/packages/$CI_PROJECT_NAME/$CI_COMMIT_TAG/$CI_PROJECT_NAME-$CI_COMMIT_TAG.tgz
